921b9a5bc6144dbd13b89e9bad6b9cd2b21f5f65
Maximo Rodriguez <maximo1225@gmail.com>
2019-11-18 19:59:11 -0400

    Creacion de usuarios por defecto

--- servicio-usuarios/src/main/java/micro/servicio/serviciousuarios/ServicioUsuariosApplication.java
+++ servicio-usuarios/src/main/java/micro/servicio/serviciousuarios/ServicioUsuariosApplication.java
@@ -1,7 +1,17 @@
 package micro.servicio.serviciousuarios;
 
+import micro.servicio.serviciousuarios.rol.Rol;
+import micro.servicio.serviciousuarios.rol.RolRepository;
+import micro.servicio.serviciousuarios.usuario.Usuario;
+import micro.servicio.serviciousuarios.usuario.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class ServicioUsuariosApplication {
@@ -10,4 +20,27 @@ public class ServicioUsuariosApplication {
         SpringApplication.run(ServicioUsuariosApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner cargarDatos(UsuarioRepository usuarioRepository, RolRepository rolRepository){
+        return args -> {
+            System.out.println("Creando los datos por defecto...");
+            System.out.println("Creando ROLES:");
+            System.out.println("1- ROLE_ANDMIN\n2- ROLE_USUARIO\n3- ROLE_EMPLEADO");
+            Rol rolAdmin = rolRepository.save(new Rol("ROLE_ADMIN"));
+            rolRepository.saveAll(Arrays.asList(
+                    new Rol("ROLE_USUARIO"),
+                    new Rol("ROLE_EMPLEADO")
+            ));
+
+            System.out.println("Creando Usuario Administrador...");
+            System.out.println("Usuario: admin");
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Admin Admin");
+            usuario.setUsername("admin");
+            usuario.setPassword(new BCryptPasswordEncoder().encode("admin"));
+            usuario.setEmail("amind@admin.com");
+            usuario.setRoles(new HashSet<>(Arrays.asList(rolAdmin)));
+            usuarioRepository.save(usuario);
+        };
+    }
 }
