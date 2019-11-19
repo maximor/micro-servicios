7a6f220da9fb6aaed8bd17182850244e6fde24db
Maximo Rodriguez <maximo1225@gmail.com>
2019-11-19 17:51:10 -0400

    Corrección: El Token siempre estaba expirado
    Existia una condición de la expiracion del token pero en nuestro caso
    no queremos que el token expire, la condision se elimino para tener el
    comportamiento deseado.

--- servicio-usuarios/src/main/java/micro/servicio/serviciousuarios/utilidades/JwtUtil.java
+++ servicio-usuarios/src/main/java/micro/servicio/serviciousuarios/utilidades/JwtUtil.java
@@ -52,6 +52,7 @@ public class JwtUtil {
 
     public Boolean validateToken(String token, UserDetails userDetails){
         final String username = extractUserName(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+//        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        return (username.equals(userDetails.getUsername()));
     }
 }
