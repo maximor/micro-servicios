3ea774a5418ab199b25f223107d50fdd5cbbd592
Maximo Rodriguez <maximo1225@gmail.com>
2019-11-20 15:15:13 -0400

    primer commit cliente web

---
+++ cliente-web/src/main/java/micro/servicio/clienteweb/ClienteWebApplication.java
@@ -0,0 +1,13 @@
+package micro.servicio.clienteweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteWebApplication.class, args);
+    }
+
+}
---
+++ cliente-web/src/test/java/micro/servicio/clienteweb/ClienteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.servicio.clienteweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
