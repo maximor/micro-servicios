7a02039444570afba3196b06dc7cb2f0d05fe663
Maximo Rodriguez <maximo1225@gmail.com>
2019-11-22 13:58:26 -0400

    Entidad para el manejo de planes y realizacion de ordenes

---
+++ cliente-web/src/main/java/micro/servicio/clienteweb/entidades/productos/Orden.java
@@ -0,0 +1,115 @@
+package micro.servicio.clienteweb.entidades.productos;
+
+import java.util.Date;
+import java.util.List;
+
+public class Orden {
+    private int id;
+    private Date fechaCreacion;
+    private Date fechaActualizacion;
+    private String email;
+    private float montoTotal;
+    private boolean abierta;
+    private boolean activa;
+    private int userId;
+
+    private List<Plan> planes;
+
+    public Orden() {
+    }
+
+    public Orden(Date fechaCreacion, Date fechaActualizacion, String email, float montoTotal, boolean abierta, boolean activa, int userId, List<Plan> planes) {
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
+        this.email = email;
+        this.montoTotal = montoTotal;
+        this.abierta = abierta;
+        this.activa = activa;
+        this.userId = userId;
+        this.planes = planes;
+    }
+
+    public Orden(int id, Date fechaCreacion, Date fechaActualizacion, String email, float montoTotal, boolean abierta, boolean activa, int userId, List<Plan> planes) {
+        this.id = id;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
+        this.email = email;
+        this.montoTotal = montoTotal;
+        this.abierta = abierta;
+        this.activa = activa;
+        this.userId = userId;
+        this.planes = planes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(Date fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(float montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public boolean isAbierta() {
+        return abierta;
+    }
+
+    public void setAbierta(boolean abierta) {
+        this.abierta = abierta;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public List<Plan> getPlanes() {
+        return planes;
+    }
+
+    public void setPlanes(List<Plan> planes) {
+        this.planes = planes;
+    }
+}
---
+++ cliente-web/src/main/java/micro/servicio/clienteweb/entidades/productos/Plan.java
@@ -0,0 +1,102 @@
+package micro.servicio.clienteweb.entidades.productos;
+
+import java.util.Date;
+
+public class Plan {
+    private int id;
+    private Date fechaCreacion;
+    private Date fechaActualizacion;
+    private String nombre;
+    private float monto;
+    private String descripcion;
+    private boolean activo;
+    private int userId;
+
+    public Plan() {
+    }
+
+    public Plan(Date fechaCreacion, Date fechaActualizacion, String nombre, float monto, String descripcion, boolean activo, int userId) {
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
+        this.nombre = nombre;
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.activo = activo;
+        this.userId = userId;
+    }
+
+    public Plan(int id, Date fechaCreacion, Date fechaActualizacion, String nombre, float monto, String descripcion, boolean activo, int userId) {
+        this.id = id;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
+        this.nombre = nombre;
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.activo = activo;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(Date fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
