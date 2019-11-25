ff25160421e417f50ad96e69c7a25429c578acdc
Maximo Rodriguez <maximo1225@gmail.com>
2019-11-25 11:30:01 -0400

    Archivo de configuracion para spring security

---
+++ cliente-web/src/main/java/micro/servicio/clienteweb/configuracion/ConfiguracionSpringSecurity.java
@@ -0,0 +1,48 @@
+package micro.servicio.clienteweb.configuracion;
+
+import micro.servicio.clienteweb.servicios.UsuarioDetailsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracionSpringSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UsuarioDetailsServices usuarioDetailsServices;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioDetailsServices).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers(
+                        "/css/**",
+                        "/js/**",
+                        "/extra-libs/**",
+                        "/images/**",
+                        "/libs/**").permitAll()
+                .antMatchers("/login*", "/registro").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .deleteCookies("JSESSIONID");
+    }
+}
