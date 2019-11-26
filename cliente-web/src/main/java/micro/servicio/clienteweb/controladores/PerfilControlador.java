e3d26f4cedeafcb2afe02f9a601fd66689eb6d10
Maximo Rodriguez <maximo1225@gmail.com>
2019-11-26 14:23:17 -0400

    Limpiando el controlador de perfil

--- cliente-web/src/main/java/micro/servicio/clienteweb/controladores/PerfilControlador.java
+++ cliente-web/src/main/java/micro/servicio/clienteweb/controladores/PerfilControlador.java
@@ -3,19 +3,15 @@ package micro.servicio.clienteweb.controladores;
 import micro.servicio.clienteweb.entidades.usuarios.CambiarContrasena;
 import micro.servicio.clienteweb.entidades.usuarios.Usuario;
 import micro.servicio.clienteweb.utilidades.RestUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 
 import java.security.Principal;
 
 @Controller
 public class PerfilControlador {
-    @Autowired
-    private RestTemplate restTemplate;
 
     @RequestMapping("/perfil")
     public String perfil(Principal principal, Model modelo){
