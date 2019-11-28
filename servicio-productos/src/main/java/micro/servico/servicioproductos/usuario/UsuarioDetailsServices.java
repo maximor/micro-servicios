f81035bddfb37b7bf759600936040c0340579042
Jean Carlos Marte <jeancmarte21@gmail.com>
2019-11-28 19:23:57 -0400

    Detalles de usuarios

---
+++ servicio-productos/src/main/java/micro/servico/servicioproductos/usuario/UsuarioDetailsServices.java
@@ -0,0 +1,23 @@
+package micro.servicio.servicioproductos.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioDetailsServices implements UserDetailsService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username);
+
+        if(usuario == null){
+            throw new UsernameNotFoundException("Usuario No encontrado: " + username);
+        }
+        return new UsuarioDetails(usuario);
+    }
+}
